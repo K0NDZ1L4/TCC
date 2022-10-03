4368d111b85b9d02e0f536809e8f45dec30a53c6
Lucas <lucasmedeiros2001@hotmail.com>
2022-10-02 22:00:49 -0300

    Criacao da funcao para extrair dados do Yahoo finance, transforma-lo em DataFrame e inserir um periodo de 30 dias futuros que serao usados na interface web.

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
-import pandas as np
+import pandas as pd
+import yfinance as yf
+from datetime import datetime, date, timedelta
+'''
+Action that being used:
+MGLU3.SA
+BBAS3.SA
+WEGE3.SA
+PETR3.SA
+'''
 
-# AJUSTE 1
-i = np.DataFrame()
-print(i)
+
+def data(name):
+    ticker = yf.Ticker(name)
+    df = pd.DataFrame(ticker.history(period='5y'))
+    df.reset_index(inplace=True)
+    df_data = pd.DataFrame({'Date': pd.date_range(df['Date'].iloc[-1] + timedelta(days=1), periods=30)})
+    df = df.append(df_data)
+    df.fillna(0, inplace=True)
+    return df
+
 
